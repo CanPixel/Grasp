5c0525ba0c01176069cacd424f81d48b0e84649f
N. Weber <ketoleperouch@gmail.com>
2019-05-20 00:22:09 +0200

    Ja nogal veel
    HDRP Pipeline, Custom Character + Animations, Custom Controller (WIP), New Level, the greatest update you have ever seen! And what's more...
    It's completely free! Except for your memory of course :(

---
+++ Assets/Scripts/ScriptableObjects/PostProcessingCollection.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace PostProcessing
+{
+    [CreateAssetMenu(menuName = "Post Processing Collection")]
+    public class PostProcessingCollection : ScriptableObject
+    {
+        public List<Material> postProcessingMaterials;
+    }
+}
