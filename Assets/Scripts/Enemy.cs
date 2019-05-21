ce300ada32ec4c63b4bf92d865521a253c871c84
Can Ur <canur11@gmail.com>
2019-05-21 15:16:51 +0200

    behavior tree beginsels

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AStar;
+using BehaviorTree;
+
+[RequireComponent(typeof(AStarUnit))]
+public class Enemy : MonoBehaviour {
+    public Transform target;
+    public float moveSpeed = 1;
+
+    [HideInInspector]
+    public BNode lastNode;
+
+    protected BNode behaviorTree;
+
+    protected Rigidbody rb;
+    
+    void Awake() {
+         StartAI();
+    }
+
+    protected void StartAI() {
+         rb = GetComponent<Rigidbody>();
+         behaviorTree = new BehaviorTree.Composite.BSequence(new BNode[]{
+                new BAction(this, CheckHealth), 
+                new BAction(this, CheckForSynergies),
+                new BAction(this, MoveInRangeOfPlayer), 
+                new BehaviorTree.Decorator.BTimer(new BehaviorTree.Composite.BSequence(new BNode[] {
+                    new BAction(this, RangedCharge), new BAction(this, RangedAttack)
+                }), 1)
+                });
+                pathfinding.SetTarget(player.transform);
+    }
+
+    protected BNode.NodeState CheckHealth() {
+        return BNode.NodeState.SUCCESS;
+    }
+
+    void FixedUpdate() {
+
+    }
+
+    protected void Move() {
+
+    }
+}
