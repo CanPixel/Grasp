746678b803156adc1b5f50c4bfe5e76109767c4e
Can Ur <canur11@gmail.com>
2019-05-23 18:17:00 +0200

    Basic AI integration

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,47 +1,97 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using AStar;
 using BehaviorTree;
 
-[RequireComponent(typeof(AStarUnit))]
 public class Enemy : MonoBehaviour {
-    public Transform target;
-    public float moveSpeed = 1;
-
-    [HideInInspector]
     public BNode lastNode;
 
-    protected BNode behaviorTree;
+    [HideInInspector]
+    public GameObject target;
+    public float moveSpeed = 1;
+    private float dir = 0, targetDir = 1;
 
+    public float targetRange = 10;
+    public float nearDistance = 3;
+    private float baseNear;
+
+    protected BNode behaviorTree;
     protected Rigidbody rb;
+    public GameObject player, playerLightPoint;
+
+    public Behavior behavior;    
+
+    [System.Serializable]
+    public enum Behavior {
+        MOTH, VAMPIRE
+    }
     
     void Awake() {
          StartAI();
     }
 
     protected void StartAI() {
-         rb = GetComponent<Rigidbody>();
-         behaviorTree = new BehaviorTree.Composite.BSequence(new BNode[]{
-                new BAction(this, CheckHealth), 
-                new BAction(this, CheckForSynergies),
-                new BAction(this, MoveInRangeOfPlayer), 
-                new BehaviorTree.Decorator.BTimer(new BehaviorTree.Composite.BSequence(new BNode[] {
-                    new BAction(this, RangedCharge), new BAction(this, RangedAttack)
-                }), 1)
-                });
-                //pathfinding.SetTarget(player.transform);
+        baseNear = nearDistance;
+        rb = GetComponent<Rigidbody>();
+        switch(behavior) {
+            default:
+            case Behavior.MOTH:
+                behaviorTree = new BehaviorTree.Composite.BSequence(new BNode[]{
+                        //new BAction(this, OnScreen), 
+                        new BAction(this, CheckTarget), 
+                        new BAction(this, Walk),
+                    //    new BehaviorTree.Decorator.BTimer(new BehaviorTree.Composite.BSequence(new BNode[] {
+                    //       new BAction(this, RangedCharge), new BAction(this, RangedAttack)
+                    //  }), 1)
+                        });
+                break;
+            case Behavior.VAMPIRE:
+                behaviorTree = new BehaviorTree.Composite.BSequence(new BNode[]{
+                        //new BAction(this, OnScreen), 
+                        new BAction(this, CheckTarget), 
+                        new BAction(this, Walk),
+                    //    new BehaviorTree.Decorator.BTimer(new BehaviorTree.Composite.BSequence(new BNode[] {
+                    //       new BAction(this, RangedCharge), new BAction(this, RangedAttack)
+                    //  }), 1)
+                        });
+                break;
+        }
     }
 
-    protected BNode.NodeState CheckHealth() {
+    protected BNode.NodeState Walk() {
+        if(target != null) MoveToTarget(target);
         return BNode.NodeState.SUCCESS;
     }
 
-    protected BNode.NodeState CheckForSynergies() {
+    protected BNode.NodeState CheckTarget() {
+        float lightDist = Vector3.Distance(transform.position, playerLightPoint.transform.position);
+        float playerDist = Vector3.Distance(transform.position, player.transform.position);
+        switch(behavior) {
+            default:
+            case Behavior.MOTH:
+                if(lightDist < targetRange) target = playerLightPoint;
+                else {
+                    if(playerDist < targetRange) target = player;
+                    else target = null;
+                }
+                break;
+            case Behavior.VAMPIRE:
+                if(playerDist < targetRange) target = player;
+                else target = null;
+                if(Flashlight.IsLightOn()) {
+                    float lightX = Mathf.Abs(playerLightPoint.transform.position.x);
+                    float posX = Mathf.Abs(transform.position.x);
+                    float playerX = Mathf.Abs(player.transform.position.x);
+                    targetDir = -1;
+                } else{
+                    targetDir = 1;
+                }
+                break;
+        }
         return BNode.NodeState.SUCCESS;
     }
 
-    protected BNode.NodeState MoveInRangeOfPlayer() {
+    protected BNode.NodeState OnScreen() {
         return BNode.NodeState.SUCCESS;
     }
 
@@ -54,10 +104,17 @@ public class Enemy : MonoBehaviour {
     }
 
     void FixedUpdate() {
-
+        behaviorTree.Run();
     }
 
-    protected void Move() {
+    protected void MoveToTarget(GameObject target) {
+        Vector3 dest = transform.position - target.transform.position;
+        if(dest.x > 0) dir = -1 * targetDir;
+        if(dest.x < 0) dir = 1 * targetDir;
+        if(Vector3.Distance(transform.position, target.transform.position) > nearDistance) move();
+    }
 
+    private void move() {
+        rb.MovePosition(rb.position + new Vector3(moveSpeed * dir, 0, 0) / 10f);
     }
 }
---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour {
+    public GameObject flashPoint;
+    public Light lightPoint;
+
+    private bool lightOn = true;
+    private static Flashlight self;
+
+    void Awake() {
+        self = this;
+    }
+
+    void Update() {
+        if(Input.GetMouseButtonDown(2)) SwitchLight(!lightOn);
+        CastLight();
+    }
+
+    public static bool IsLightOn() {
+        return self.lightOn;
+    }
+
+    protected void CastLight() {
+        RaycastHit hit;
+        bool rayHit = Physics.Raycast(lightPoint.transform.position, lightPoint.transform.forward, out hit, 20);
+        if(lightOn && rayHit && hit.collider.gameObject != null && hit.collider.gameObject.tag != "Light") flashPoint.transform.position = hit.point;
+        if(!rayHit || !lightOn) flashPoint.transform.position = new Vector3(0, -100, 0);
+    }
+
+    public void SwitchLight(bool i) {
+        lightOn = i;
+        lightPoint.enabled = i;
+    }
+}
