0a0186dced7a40a6253858503849516a64e3ffd4
N. Weber <ketoleperouch@gmail.com>
2019-05-29 14:25:40 +0200

    Initial Local Commit

---
+++ Assets/Scripts/ScriptableObjects/AnimationInitializer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Animation Initializer")]
+public class AnimationInitializer : ScriptableObject
+{
+    public string clipName = "Animation Clip";
+    public TransformSet transform;
+}
+
+[System.Serializable]
+public struct TransformSet
+{
+    public Vector3 localPosition;
+    public Vector3 localEulerAngles;
+    public Vector3 localScale;
+}
