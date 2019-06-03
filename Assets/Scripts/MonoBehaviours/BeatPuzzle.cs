967004870b42e426f1ec2e869b7d5cbcfda5da8b
Can Ur <canur11@gmail.com>
2019-06-03 16:54:21 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/MonoBehaviours/BeatPuzzle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatPuzzle : MonoBehaviour {
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+
+    void OnTriggerEnter(Collider col) {
+        if(col.tag == "Player") {
+            var controller = col.GetComponent<PlayerController>();
+            controller.lockControls = true;
+        }
+    }
+}
