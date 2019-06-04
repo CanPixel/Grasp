bf1712beeaf91040832da42311e8a58afa1a73bb
Can Ur <canur11@gmail.com>
2019-06-04 13:05:03 +0200

    ik weet niet waarom ik dit push

--- Assets/Lighting-Fog/Shaders/Beam Behaviour.cs
+++ Assets/Lighting-Fog/Shaders/Beam Behaviour.cs
@@ -21,13 +21,13 @@ public class BeamBehaviour : MonoBehaviour
         _bars = new GameObject[_instanceCount];
         for (var i = 0u; i < _instanceCount; i++)
         {
-            var y = (Random.Value01(i + _randomSeed) - 0.5f) * _height;
-            _bars[i] = Instantiate(_template, transform);
-            _bars[i].transform.localPosition = new Vector3(0, y, 0);
-            _bars[i].GetComponent<Light>().color =
-                Color.HSVToRGB(Random.Value01(i + _randomSeed + 4000u), 0.8f, 1);
-            _bars[i].GetComponentInChildren<Renderer>().material.SetColor("_EmissiveColor",
-                Color.HSVToRGB(Random.Value01(i + _randomSeed + 4000u), 0.8f, 1) * 5);
+   //         var y = (Random.Value01(i + _randomSeed) - 0.5f) * _height;
+       //     _bars[i] = Instantiate(_template, transform);
+        //    _bars[i].transform.localPosition = new Vector3(0, y, 0);
+         //   _bars[i].GetComponent<Light>().color =
+ //               Color.HSVToRGB(Random.Value01(i + _randomSeed + 4000u), 0.8f, 1);
+//            _bars[i].GetComponentInChildren<Renderer>().material.SetColor("_EmissiveColor",
+ //               Color.HSVToRGB(Random.Value01(i + _randomSeed + 4000u), 0.8f, 1) * 5);
         }
         Destroy(_template);
 
@@ -40,8 +40,8 @@ public class BeamBehaviour : MonoBehaviour
         for (var i = 0u; i < _instanceCount; i++)
         {
             var p = _bars[i].transform.localPosition;
-            var spd = (0.5f + Random.Value01(i + _randomSeed + 10000u)) * _speed;
-            p.x = ((spd * t) % _width) - _width * 0.5f;
+   //         var spd = (0.5f + Random.Value01(i + _randomSeed + 10000u)) * _speed;
+     //       p.x = ((spd * t) % _width) - _width * 0.5f;
             _bars[i].transform.localPosition = p;
         }
 
