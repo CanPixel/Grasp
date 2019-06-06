a975e64c3cd331dab5714dd7c8be352957be4458
Can Ur <canur11@gmail.com>
2019-06-06 14:51:06 +0200

    bolderroop klaar

---
+++ Assets/Scripts/MonoBehaviours/DestroyAfterDelay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterDelay : MonoBehaviour {
+    public float delay = 1;
+
+    public bool start = false;
+
+    void Start() {
+        if(start) DestroyOBJ();
+    }
+
+    public void DestroyOBJ() {
+       // Destroy(gameObject, delay);
+    }
+}
