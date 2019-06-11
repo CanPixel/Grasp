592cb7968626133a33a28a6c9ffcd5aefa24aebd
Can Ur <canur11@gmail.com>
2019-06-11 11:54:52 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/MonoBehaviours/Audio/EnemySound.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySound : MonoBehaviour {
+    public AudioSource scream;
+
+    public void Scream() {
+        if(scream.isPlaying) return;
+        scream.Play();
+    }
+}
